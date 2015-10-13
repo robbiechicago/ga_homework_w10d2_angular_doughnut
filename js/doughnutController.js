4c30fc843be25de14e5a774cd4eecff90f209c67
Rob Holloway <robbiechicago@gmail.com>
2015-10-13 18:39:18 +0100

    things

---
+++ js/doughnutController.js
@@ -0,0 +1,52 @@
+angular.module('doughApp', [])
+  .controller('DoughnutController', DoughnutController);
+
+DoughnutController.$inject = ['$http'];
+
+function DoughnutController($http) {
+
+  var self = this;
+
+  self.all = [];
+
+  function getDoughnuts() {
+    $http
+      .get('http://api.doughnuts.ga/doughnuts')
+      .then(function(response) {
+        self.all = response.data
+      });
+  };
+
+  getDoughnuts();
+
+  self.addNut = addNut;
+  self.newNut = {};
+
+  function addNut() {
+    $http
+    .post('http://api.doughnuts.ga/doughnuts', self.newNut)
+    .then(function(response) {
+      console.log(response.data);
+      // getNuts();
+      self.all.push(response.data)
+    });
+  };
+
+  self.deleteNut = deleteNut;
+  function deleteNut(id) {
+
+  }
+
+  
+
+
+}
+
+
+
+
+
+
+
+
+
